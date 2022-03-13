cc4cd2866e2d70214b7dfe6611850907452e6ad3
aflahu <clouderka@gmail.com>
2022-03-13 23:54:30 +0700

    Add App.test at src

---
+++ src/App.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Images from "./components/Images";
+import axios from "axios";
+
+test("appbar should to be in the document", () => {
+  render(<App />);
+  const appbar = screen.getByText(/React-Flickr/i);
+  expect(appbar).toBeInTheDocument();
+});
+
+test("See at flickr should to be in the document", async () => {
+  let links = [];
+  const data = await axios("http://localhost:4040/api/public-photos").then(
+    (res) => res.data
+  );
+  render(<Images data={data.data} />);
+
+  links = screen.getAllByText(/See at flickr/i);
+  links.forEach((link) => {
+    expect(link).toBeInTheDocument();
+  });
+});
