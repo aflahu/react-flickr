bcb6e7a519600f25af330964737f13dea64b0c0b
aflahu <clouderka@gmail.com>
2022-03-18 10:25:14 +0700

    Add RecoilRoot at src/App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,14 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import Images from "./components/Images";
 import axios from "axios";
+import { RecoilRoot } from "recoil";
 
 test("appbar should to be in the document", () => {
-  render(<App />);
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  );
   const appbar = screen.getByText(/React-Flickr/i);
   expect(appbar).toBeInTheDocument();
 });
